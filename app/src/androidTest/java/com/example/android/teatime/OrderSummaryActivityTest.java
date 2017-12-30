7047a113e961a7aa1848390f8a1694d5a70dd5ab
jadintasalsabilah <jadintasalsabilahh@gmail.com>
2017-12-30 12:08:58 +0700

    finishing

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -16,22 +16,32 @@
 
 package com.example.android.teatime;
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
-public class OrderSummaryActivityTest {
+import android.app.Activity;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
-    // TODO (2) Add the rule that indicates we want to use Espresso-Intents APIs in functional UI tests
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-
-    // TODO (3) Finish this method which runs before each test and will stub all external
-    // intents so all external intents will be blocked
+import static android.app.Instrumentation.ActivityResult;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
 
     @RunWith(AndroidJUnit4.class)
     public class OrderSummaryActivityTest {
 
         private static final String emailMessage = "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!";
-
-
-    public void clickSendEmailButton_SendsEmail() {
 
         @Rule
         public IntentsTestRule<OrderSummaryActivity> mActivityRule = new IntentsTestRule<>(
@@ -45,6 +55,7 @@ public class OrderSummaryActivityTest {
         }
 
         @Test
+        public void clickSendEmailButton_SendsEmail() {
 
                 onView(withId(R.id.send_email_button)).perform(click());
         // intended(Matcher<Intent> matcher) asserts the given matcher matches one and only one
